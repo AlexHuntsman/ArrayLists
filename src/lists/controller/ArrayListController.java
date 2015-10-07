00fe7dd2f2f9108fbf2d9d204b583bab55fe3beb
Alexander Huntsman <Huntsman.alex101@gmail.com>
2015-10-07 09:59:46 -0600

    Added Words to my array list and display them into a popup.

--- src/lists/controller/ArrayListController.java
+++ src/lists/controller/ArrayListController.java
@@ -1,9 +1,30 @@
 package lists.controller;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 	public class ArrayListController
 	{
+			private ArrayList<String> firstWords;
+			
+			public ArrayListController()
+			{
+				firstWords = new ArrayList<String>();
+			}
+			
 		public void start()
+		{	
+			firstWords.add("ehtwgf weyfugweauyfweauigf");
+			firstWords.add("bababa babab abba ab abababab BA!");
+			showList();
+		}
+		
+		private void showList()
 		{
+			for(int spot = 0; spot < firstWords.size(); spot++)
+			{
+				JOptionPane.showMessageDialog(null, firstWords.get(spot) + " is at spot#" + spot);
+			}
 			
 		}
 
